32c45e0f3c830af70c6e57696aef71fb13c11cf4
Neri Bez Fontana <nerifontana@gmail.com>
2017-08-25 15:22:56 -0300

    refactor: alterado a validacao de cpf e cnpj para fazer uso do modulo cpf_cnpj

--- src/components/cpf/cpf.validator.ts
+++ src/components/cpf/cpf.validator.ts
@@ -1,57 +1,12 @@
 import {AbstractControl, ValidatorFn} from "@angular/forms";
 
-export function cpfValidationFn(blockEqualNumberSequence?: boolean): ValidatorFn {
+const CPF = require("cpf_cnpj").CPF;
+
+export function cpfValidationFn(): ValidatorFn {
   return (control: AbstractControl) => {
     let value = control.value;
-    blockEqualNumberSequence = blockEqualNumberSequence || true;
 
-    if (!value) {
-      return null;
-    }
-
-    let allNumbersAreEqual: boolean = value === '00000000000' ||
-      value === '11111111111' || value === '22222222222' || value === '33333333333' ||
-      value === '44444444444' || value === '55555555555' || value === '66666666666' ||
-      value === '77777777777' || value === '88888888888' || value === '99999999999';
-
-    if (allNumbersAreEqual && blockEqualNumberSequence) {
-      return {
-        cpf: true
-      };
-    }
-
-    let soma = 0;
-    let resto;
-
-    for (let i = 1; i <= 9; i++) {
-      soma += parseInt(value.substring(i - 1, i)) * (11 - i);
-    }
-
-    resto = (soma * 10) % 11;
-
-    if ((resto === 10) || (resto === 11)) {
-      resto = 0;
-    }
-
-    if (resto !== parseInt(value.substring(9, 10))) {
-      return {
-        cpf: true
-      };
-    }
-
-    soma = 0;
-
-    for (let i = 1; i <= 10; i++) {
-      soma += parseInt(value.substring(i - 1, i)) * (12 - i);
-    }
-
-    resto = (soma * 10) % 11;
-
-    if ((resto === 10) || (resto === 11)) {
-      resto = 0;
-    }
-
-    if (resto !== parseInt(value.substring(10, 11))) {
+    if (!CPF.isValid(value)) {
       return {
         cpf: true
       };
