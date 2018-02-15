a490ddc6978bb4466fcd22bbe23367e66f9e4670
Neri Bez Fontana <nerifontana@gmail.com>
2018-02-15 18:31:53 -0200

    Ajustes

--- public_api.ts
+++ public_api.ts
@@ -1,13 +1,1 @@
 export * from './src/app/ngx-br.module';
-
-export * from './src/app/components/cpf/cpf.module';
-export * from './src/app/components/cep/cep.module';
-export * from './src/app/components/cnpj/cnpj.module';
-export * from './src/app/components/dinheiro/dinheiro.module';
-export * from './src/app/components/estados/estados.module';
-export * from './src/app/components/hora/hora.module';
-export * from './src/app/components/percentual/percentual.module';
-export * from './src/app/components/peso/peso.module';
-export * from './src/app/components/telefone/telefone.module';
-
-export * from './src/app/pipes/pipes.module';
