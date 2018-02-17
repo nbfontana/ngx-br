34c7852afdf38052ca638c48fd7f059b32e86fda
Neri Bez Fontana <nerifontana@gmail.com>
2018-02-17 15:19:00 -0200

    test: Atualização nas versões do protractor e testes e2e

--- src/e2e/app.e2e-spec.ts
+++ src/e2e/app.e2e-spec.ts
@@ -9,6 +9,7 @@ describe('ngx-br App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Form');
   });
+
 });
